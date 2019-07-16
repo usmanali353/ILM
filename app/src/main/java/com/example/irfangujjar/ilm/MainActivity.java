12b567dea5ca233eedadcc294e70b6017d3d6b1a
Usman <aliusman353@gmail.com>
2019-07-16 14:49:18 +0500

    Initial Commit

--- app/src/main/java/com/example/irfangujjar/ilm/MainActivity.java
+++ app/src/main/java/com/example/irfangujjar/ilm/MainActivity.java
@@ -219,7 +219,6 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
-
         alert.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
